a2fb1ac8ea28bab064afa3c9c40ede9ae747ae63
RaeHwan Park <prh0108@naver.com>
2025-12-21 14:39:47 +0900

    fix: entrypoint.sh 수정

--- prod/alertmanager/entrypoint.sh
+++ prod/alertmanager/entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh
 
-# 1. 템플릿 파일(.tmp)을 읽어 환경변수를 치환한 뒤 실제 설정 파일로 저장
+# 1. Critical 및 Warning 웹훅 URL 각각 치환
+sed -i "s|\${SLACK_CRIT_WEBHOOK_URL}|$SLACK_CRIT_WEBHOOK_URL|g" /etc/alertmanager/alertmanager.yml.tmp
+sed -i "s|\${SLACK_WARN_WEBHOOK_URL}|$SLACK_WARN_WEBHOOK_URL|g" /etc/alertmanager/alertmanager.yml.tmp
 
-# 슬래시(/)가 포함된 URL 처리를 위해 구분자를 | 로 사용
-sed "s|\${SLACK_WEBHOOK_URL}|$SLACK_WEBHOOK_URL|g" /etc/alertmanager/alertmanager.yml.tmp > /etc/alertmanager/alertmanager.yml
+# 2. 치환된 내용을 최종 설정 파일로 복사
+cp /etc/alertmanager/alertmanager.yml.tmp /etc/alertmanager/alertmanager.yml
 
-# 2. 원래 실행하려던 Alertmanager 실행
-exec /bin/alertmanager --config.file=/etc/alertmanager/alertmanager.yml
+# 3. Alertmanager 실행
+exec /bin/alertmanager --config.file=/etc/alertmanager/alertmanager.yml "$@"
